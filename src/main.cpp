d065ad1105674fcee69fa4c8d12999ce056f2cb4
lcd0327 <lcd0327@gmail.com>
2024-05-13 05:08:51 +0800

    fix: main wrong push

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@
 #include "gl.h"
 #include "json.h"
 #include "network.h"
+#include <cassert>
 
 int main(int argc, char* argv[]) {
     std::vector<int> config = read_json(std::filesystem::path("config.json"));
 
     Board game(8, 8, 10);
-    game.gl_main_menu();
+    // game.gl_main_menu();
+    
+    GL gl;
+    int result = gl.main_menu();
+    assert(result == 0);
     return 0;
 }
