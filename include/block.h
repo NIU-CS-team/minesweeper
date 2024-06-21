6bc0583411e9786bb03cac8d6bd6b2a77fcf0166
jay940721 <mickey.chou2005@gmail.com>
2024-06-21 21:57:09 +0800

    refactor(block): change state type to block_state

--- include/block.h
+++ include/block.h
@@ -6,7 +6,7 @@ enum block_value { EMPTY, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, MINE }
 
 struct block {
     const float size = 0.02f;
-    int state = HIDDEN;  // 0: hidden, 1: revealed, 2: flagged
+    block_state state = HIDDEN;
     int value = EMPTY;  // 0: empty, 1-8: number of mines around, 9: mine
     double gl_x;
     double gl_y;
