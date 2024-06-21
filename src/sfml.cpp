acc476567941a42aa66e40f1e67b83f401f47b10
jay940721 <mickey.chou2005@gmail.com>
2024-06-21 23:07:14 +0800

    feat(sfml): add init() function

--- src/sfml.cpp
+++ src/sfml.cpp
@@ -3,10 +3,15 @@
 int SFML::init() {
     window.create(sf::VideoMode(800, 800), "Minesweeper");
     window.setFramerateLimit(60);
-    if (!font.loadFromFile("Cubic_11_1.100_R.ttf")) {
+
+    if(!window.isOpen()) {
+        std::cerr << "Failed to create window" << std::endl;
+        return -1;
+    }
+    if (!font.loadFromFile("../font/Cubic_11_1.100_R.ttf")) {
         std::cerr << "Failed to load font" << std::endl;
         return -1;
     }
-    
+
     return 0;
 }
