455912af04f072e16501ce3950ad96058859659d
jay940721 <mickey.chou2005@gmail.com>
2024-06-22 00:14:09 +0800

    style(sfml.cpp): use clang-format

--- src/sfml.cpp
+++ src/sfml.cpp
@@ -4,7 +4,7 @@ int SFML::init() {
     window.create(sf::VideoMode(800, 800), "Minesweeper");
     window.setFramerateLimit(60);
 
-    if(!window.isOpen()) {
+    if (!window.isOpen()) {
         std::cerr << "Failed to create window" << std::endl;
         return -1;
     }
@@ -12,7 +12,7 @@ int SFML::init() {
         std::cerr << "Failed to load font" << std::endl;
         return -1;
     }
-    if(!texture.loadFromFile("../image/spritesheet.png")) {
+    if (!texture.loadFromFile("../image/spritesheet.png")) {
         std::cerr << "Failed to load texture" << std::endl;
         return -1;
     }
