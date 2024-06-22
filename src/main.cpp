0d71a9f15024ff563da747129c3b15a13ed977ee
jay940721 <mickey.chou2005@gmail.com>
2024-06-22 13:08:19 +0800

    refactor(main): update to use SFML

--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,12 @@
 #include <iostream>
 
 #include "board.h"
-#include "gl.h"
+#include "sfml.hpp"
 #include "config.h"
 #include "network.h"
 
 int main(int argc, char* argv[]) {
-    Board game(HEIGHT, WIDTH, MINES);
-
-    GL gl;
-    gl.init();
-    std::string ip;
-    while (true) {
-        int menuSelection = gl.main_menu();
-        switch (menuSelection) {
-            case 1:
-                gl.clear();
-                game.clear();
-                game.generate_mines();
-                gl.host_game(game, 6969, 2);
-                break;
-            case 2:
-                std::cout << "Joining game, input ip: " << std::endl;
-                std::cin >> ip;
-                uint32_t host_address;
-                inet_pton(AF_INET, ip.c_str(), &host_address);
-                gl.join_game(host_address, 6969);
-                break;
-            case 3:
-                gl.clear();
-                game.clear();
-                game.generate_mines();
-                gl.play_single(game);
-                break;
-            default:
-                assert(false);
-        }
-    }
-
-    while (!glfwWindowShouldClose(gl.window)) {
-        glfwPollEvents();
-    }
-
-    // gl.play_single(game);
+    SFML game(HEIGHT, WIDTH, MINES);
+    game.init();
     return 0;
 }
